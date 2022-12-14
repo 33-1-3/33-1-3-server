7bfad4ae454fd46e23eae8e6a3312d766b2d5275
GuRaBang <qkrdbsgk00@gmail.com>
2022-12-14 23:46:49 +0900

    FEAT: db연결 및 express 동작 확인 테스트

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+try {
+  mongoose
+    .connect(process.env.DB_URL)
+    .then(() => console.log('MongoDB Connected'), {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+    .catch((error) => console.error(error));
+} catch (error) {
+  console.error(error);
+}
 const app = express();
+app.use(express.json());
+const Auth = require('./models/Auth');
+
+app.post('/test', async (req, res) => {
+  console.log(Auth);
+  try {
+    const auth = await new Auth(req.body);
+    console.log(auth._id);
+    await auth.save();
+    res.send('good');
+  } catch (err) {
+    console.log(err);
+  }
+});
+const User = require('./models/User');
+
+app.post('/test/user', async (req, res) => {
+  console.log(User);
+  try {
+    const user = await new User(req.body);
+    await user.save();
+    res.send('good');
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.listen(process.env.PORT || 5000, () => {
+  console.log('server started');
+});
