41223e89651f98b3f23c3ef2dae3c3bf2970f567
GuRaBang <qkrdbsgk00@gmail.com>
2022-12-19 16:00:35 +0900

    FEAT: 사용자 비밀번호 암호화
    - bcrypt 설치
    - 회원가입 후 db 저장 전 암호화
    - 로그인 시 암호화된 비밀번호와 비교

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const nodemailer = require('nodemailer');
 const ejs = require('ejs');
+const bcrypt = require('bcrypt');
 
 const cors = require('cors');
 
@@ -40,32 +41,33 @@ const { Auth, Collection, UserVinyl, CommonVinyl } = require('./models');
 app.post('/signin', async (req, res) => {
   try {
     const { email, password } = await req.body;
-    if (!email || !password) {
-      return res.status(401).send({ error: '값을 입력해라' });
-    }
 
-    const user = await Auth.findOne({ email, password });
-
+    const user = await Auth.findOne({ email });
     if (!user) return res.status(200).send({ state: 'notExist' });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) return res.status(200).send({ state: 'notExist' });
     if (user.auth === false) return res.status(200).send({ state: 'needAuth' });
-    res.status(200).send({
+    return res.status(200).send({
       userId: user.id,
     });
-  } catch (err) {
-    return res.status(401).send(err);
+  } catch (error) {
+    return res.status(401).send({ error });
   }
 });
 
 // 회원가입
 app.post('/signup', async (req, res) => {
   try {
-    const { email, nickname } = req.body;
+    const { email, nickname, password } = req.body;
     const ExistedUser = await Auth.findOne({ email });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     if (ExistedUser) {
       return res.send({ state: 'duplicate' });
     }
-    const user = new Auth(req.body);
+    const user = new Auth({ email, nickname, password: hashedPassword });
     await user.save();
     const userId = user.id;
     // 이메일 전송
@@ -97,9 +99,9 @@ app.post('/signup', async (req, res) => {
       html: emailTemplate,
     });
 
-    res.send({ state: 'success' });
+    return res.send({ state: 'success' });
   } catch (err) {
-    return res.status(401).send({ error: err });
+    return res.status(401).send(err);
   }
 });
 
@@ -108,9 +110,9 @@ app.post('/verification', async (req, res) => {
   try {
     const { userId } = req.body;
     await Auth.findOneAndUpdate({ _id: userId }, { $set: { auth: true } });
-    res.send('success');
-  } catch (err) {
-    return res.status(401).send({ error: 'error' });
+    return res.send('success');
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -128,9 +130,9 @@ app.get('/collections/:userId/:releasedId', async (req, res) => {
       return { title, isChecked };
     });
 
-    res.send(response);
-  } catch (err) {
-    res.status(400).send(err);
+    return res.send(response);
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -146,9 +148,9 @@ app.get('/collections/:userId', async (req, res) => {
         vinylCount: vinyls.length,
       })
     );
-    res.send(response);
-  } catch (err) {
-    console.log(err);
+    return res.send(response);
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -159,9 +161,9 @@ app.post('/collections/:userId', async (req, res) => {
     const { title } = req.body;
     const collection = new Collection({ title, userId });
     await collection.save();
-    res.send(collection.id);
-  } catch (err) {
-    console.log(err);
+    return res.send(collection.id);
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -174,9 +176,9 @@ app.put('/collections/:collectionId', async (req, res) => {
       { _id: collectionId },
       { $set: { title } }
     );
-    res.end();
-  } catch (err) {
-    console.log(err);
+    return res.end();
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -185,9 +187,9 @@ app.delete('/collections/:collectionId', async (req, res) => {
   try {
     const { collectionId } = req.params;
     await Collection.findOneAndDelete({ _id: collectionId });
-    res.end();
-  } catch (err) {
-    console.log(err);
+    return res.end();
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -210,9 +212,9 @@ app.get('/collection/:collectionId', async (req, res) => {
         released,
       })
     );
-    res.send({ collectionTitle, vinylsInfo });
-  } catch (err) {
-    console.log(err);
+    return res.send({ collectionTitle, vinylsInfo });
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -264,9 +266,9 @@ app.post('/vinyl/:userId', async (req, res) => {
       await newVinyl.save();
     }
 
-    res.status(201).send();
-  } catch (err) {
-    res.status(400).send(err);
+    return res.status(201).send();
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -285,9 +287,9 @@ app.put('/userVinyl/:collectionId/:releasedId', async (req, res) => {
       { new: true }
     );
     if (!updatedCollection) res.status(404).send();
-    res.send(updatedCollection);
-  } catch (err) {
-    res.status(400).send(err);
+    return res.send(updatedCollection);
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -298,9 +300,9 @@ app.get('/userVinyl/:userId/:releasedId', async (req, res) => {
   try {
     const userVinyl = await UserVinyl.findOne({ userId, releasedId });
     if (!userVinyl) res.status(404).send();
-    res.send({ info: userVinyl.info, memo: userVinyl.memo });
-  } catch (err) {
-    res.status(500).send(err);
+    return res.send({ info: userVinyl.info, memo: userVinyl.memo });
+  } catch (error) {
+    return res.status(500).send({ error });
   }
 });
 
@@ -315,9 +317,9 @@ app.put('/commonVinyl/:releasedId', async (req, res) => {
       { new: true }
     );
     if (!updatedVinyl) res.status(404).send();
-    res.send(updatedVinyl);
-  } catch (err) {
-    res.status(400).send(err);
+    return res.send(updatedVinyl);
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
