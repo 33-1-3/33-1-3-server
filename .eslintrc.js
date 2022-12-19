bf77148cce00406d72f5af6a1f68d9f0c5f38624
GuRaBang <qkrdbsgk00@gmail.com>
2022-12-19 21:01:03 +0900

    CHORE: ESLint 설정 수정 및 추가
    - extension 순서 변경
    - underscore 사용하기 위해 규칙 off

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'airbnb-base'],
+  parserOptions: { ecmaVersion: 'latest' },
+  extends: ['eslint:recommended', 'airbnb-base', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
     'linebreak-style': 0,
+    'no-underscore-dangle': 'off',
   },
 };
